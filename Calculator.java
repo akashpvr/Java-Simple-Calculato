5f059f92a10444571faed68646400453da66d80c
akashpvr <pakashvarma2374@gmail.com>
2025-10-15 23:33:40 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,30 @@
+// File: Calculator.java
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter first number:");
+        double num1 = sc.nextDouble();
+
+        System.out.println("Enter second number:");
+        double num2 = sc.nextDouble();
+
+        System.out.println("Choose an operation (+, -, *, /):");
+        char op = sc.next().charAt(0);
+
+        double result = 0;
+
+        switch (op) {
+            case '+': result = num1 + num2; break;
+            case '-': result = num1 - num2; break;
+            case '*': result = num1 * num2; break;
+            case '/': result = num1 / num2; break;
+            default: System.out.println("Invalid operation!");
+        }
+
+        System.out.println("Result: " + result);
+        sc.close();
+    }
+}
